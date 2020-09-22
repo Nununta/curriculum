d75cec45ac692956c3338614bc40c81fcfc9a332
“YutaYuki” <yuta@example.com>
2020-09-22 15:44:13 +0900

    9/22　課題提出

---
+++ 2-12/index.php
@@ -0,0 +1,32 @@
+<form action="result.php" method="post">
+お名前：<input type="text" name= "my_name" />
+<br>
+ご希望商品：
+<input type="radio" name="item" value="A賞">A賞
+<input type="radio" name="item" value="B賞">B賞
+<input type="radio" name="item" value="C賞">C賞
+<br>
+個数：
+<select name="quantity">
+<?php for($i=1; $i<=10; $i++) { ?>
+<option value="<?php echo $i; ?>">
+<?php echo $i; ?>
+</option>
+<?php } ?>
+</select>
+<br>
+<input type="submit" value="申込">
+</form>
+
+
+<?php
+echo "<br>";
+echo "---------------------------------------------------------------------------"."<br>";
+echo "モジュール：機能単位、交換可能な構成部分などを意味する。機器やシステムの一部を構成するひとまとまりの機能を持った部品。"."<br>";
+echo "<br>";
+echo "バージョン管理システム：コンピュータ上で作成および編集されるファイルの変更履歴を管理するためのシステム。"."<br>".
+"ソフトウェア開発上では、ソースコードの管理に使用される。(Gitなど。)"."<br>";
+echo "<br>";
+echo "サブクエリ:「副問い合わせ」とも呼ばれ、SQL文の中にSELECT文を埋め込み、SELECT文の結果を利用した抽出を行うことが可能。"."<br>";
+echo "---------------------------------------------------------------------------"."<br>";
+?>
---
+++ 2-12/result.php
@@ -0,0 +1,11 @@
+<?php
+$my_name = $_POST['my_name'];
+$item = $_POST['item'];
+$quantity = $_POST['quantity'];
+?>
+
+<p>お名前：<?php echo $my_name; ?></p>
+<p>ご希望景品：<?php echo $item; ?></p>
+<p>個数：<?php echo $quantity ?></p>
+
+
